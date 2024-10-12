2c71e222a4769bb82f811e66b8f8425ad2c2a48b
special-shaped <yixing13600352578@163.com>
2024-10-12 21:45:09 +0800

    20241012

---
+++ c2/src/main/java/org/example/c2/C2Application.java
@@ -0,0 +1,10 @@
+package org.example.c2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(C2Application.class, args);
+    }
+}
